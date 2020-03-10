90768b83dc736601142266ae46ab19be66b3bcd8
Trenton Wesche <TrWesche@live.com>
2020-03-09 23:45:05 -0500

    js react pass bus. info app to business component

--- ravenous/src/App/App.js
+++ ravenous/src/App/App.js
@@ -5,13 +5,27 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business];
+
 // It looks like they have updated the method of creating components for the newest version of reach to use the function keyword instead?  Both of these work.
 function App() {
   return (
     <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
     </div>
   );
 }
